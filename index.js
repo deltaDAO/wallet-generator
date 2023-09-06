42b72385b5851ac5a71c025c995648af6d18b419
Moritz Kirstein <mkirstein@protonmail.com>
2023-09-06 13:45:48 +0200

    fix: crashes

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-const {Wallet} = require('ethers')
+const { Wallet } = require('ethers')
 const fs = require('fs')
-
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+  
 const WALLET_EXPORT_FILE = './wallets.json'
 
 function main() {
-    const newWallet = Wallet.createRandom()
+    
+    readline.question(`What do you want to name the wallet? This can be helpful for future reference. (Return to skip)`, input => {
+        const newWallet = Wallet.createRandom()
 
-    try {
-        fs.readFileSync(WALLET_EXPORT_FILE)
-    }
-    catch {
-        fs.writeFileSync(WALLET_EXPORT_FILE, JSON.stringify([]))
-    }
+        let wallets
+        try {
+            wallets = JSON.parse(fs.readFileSync(WALLET_EXPORT_FILE))
+        } catch {
+            wallets = []
+        }
 
-    const wallets = JSON.parse(fs.readFileSync(WALLET_EXPORT_FILE))
+        if(!Array.isArray(wallets)) wallets = []
 
-    wallets.push({
-        address: newWallet.address,
-        privateKey: newWallet.privateKey,
-        mnemonic: newWallet.mnemonic
-    })
+        wallets.push({
+            identifier: input,
+            address: newWallet.address,
+            privateKey: newWallet.privateKey,
+            mnemonic: newWallet.mnemonic
+        })
 
-    fs.writeFileSync(WALLET_EXPORT_FILE, JSON.stringify(wallets))
+        fs.writeFileSync(WALLET_EXPORT_FILE, JSON.stringify(wallets, null, 2))
 
-    console.log(`New wallet created with address ${newWallet.address}. See ${WALLET_EXPORT_FILE}`)
+        console.log(`New wallet created with address ${newWallet.address}. See ${WALLET_EXPORT_FILE}`)
+        
+        readline.close();
+    });
+    
 }
 
 main()
